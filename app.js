9516f09a0131b7c8ab1d008df62193eb8d30eefd
bryanlampert <bryanlampert@gmail.com>
2017-12-04 15:34:58 +0000

    Adding HTTP Requests with NodeJS exercise. Including step1

---
+++ app.js
@@ -0,0 +1,23 @@
+var https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, (response) => {
+
+    response.setEncoding('utf-8');
+
+    response.on('data', (chunk) => {
+      console.log('Chunk received. Length: \n', chunk.length);
+    });
+
+    response.on('end', function() {
+      console.log('Response stream complete');
+    });
+
+  });
+}
